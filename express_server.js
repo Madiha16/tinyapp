47a26f95959eccdb801542256a88062ba1b02995
Your Name <your@email.com>
2022-02-25 02:30:59 -0500

    Save user's hashed passwords for testing purposes

--- express_server.js
+++ express_server.js
@@ -30,12 +30,12 @@ const users = {
   "aa": {
     id: "aa",
     email: "a@a",
-    password: "a"
+    password: "$2a$10$O6a3j/1KBeXwxRMGXtWFQ.tnDZEMwzp0NxI1rfH8C0eLT7d1sfc6C"
   },
  "bb": {
     id: "bb",
     email: "b@b",
-    password: "b"
+    password: "$2a$10$5BW3UBhoNgDaq5OWgntdEeagJ8bQotNxRH3ZhiT9b.vXDlV1M1qJe"
   }
 };
 
